05f470cd990d88cd264da147eabb4088db38d4a4
matheus-nb <matheus.nb@gmail.com>
2022-11-21 09:09:58 -0400

    Final Test

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Exemplo de argoCD!!</h1>"))
+		w.Write([]byte("<h1>MeetUP - <strong>ArgoCD</strong></h1>"))
 	})
 
 	http.ListenAndServe(":8080", nil)
